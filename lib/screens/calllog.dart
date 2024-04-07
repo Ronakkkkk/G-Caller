114defbed2ba69acf4c2e6d6f823e522fa1f1759
Ronak <ronak01.raj@gmail.com>
2024-04-08 03:54:45 +0545

    update

--- lib/screens/calllog.dart
+++ lib/screens/calllog.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:call_log/call_log.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:intl/intl.dart';
 
 import '../widgets/bottom_navigation_bar.dart';
 
@@ -41,6 +42,112 @@ class _CallLogScreenState extends State<CallLogScreen> {
     }
   }
 
+  String _formatTimestamp(int? timestamp) {
+    if (timestamp == null) {
+      return 'Unknown';
+    }
+
+    final now = DateTime.now();
+    final today = DateTime(now.year, now.month, now.day);
+    final yesterday = DateTime(now.year, now.month, now.day - 1);
+
+    final callTime = DateTime.fromMillisecondsSinceEpoch(timestamp);
+
+    if (callTime.isAtSameMomentAs(today)) {
+      final formatter = DateFormat.jm(); // Show only time
+      return 'Today ${formatter.format(callTime)}';
+    } else if (callTime.isAtSameMomentAs(yesterday)) {
+      return 'Yesterday';
+    } else {
+      final difference = now.difference(callTime);
+      if (difference.inDays == 1) {
+        return '1 day ago';
+      } else {
+        return '${difference.inDays} days ago';
+      }
+    }
+  }
+
+  Widget _buildCallLogTile(CallLogEntry callLog) {
+    String callType = '';
+    Color callTypeColor = Colors.green;
+    late IconData callTypeIcon;
+
+    if (callLog.callType == CallType.incoming) {
+      callType = 'Incoming';
+      callTypeColor = Colors.blue;
+      callTypeIcon = Icons.call_received_outlined;
+    } else if (callLog.callType == CallType.missed) {
+      callType = 'Missed';
+      callTypeColor = Colors.red;
+      callTypeIcon = Icons.call_missed_outlined;
+    } else {
+      callType = 'Outgoing';
+      callTypeIcon = Icons.call_made_outlined;
+    }
+
+    final formattedTimestamp = _formatTimestamp(callLog.timestamp);
+
+    return ListTile(
+      leading: const CircleAvatar(
+        // You can display a photo here if available
+        child: Icon(Icons.person),
+      ),
+      title: Text(
+        callLog.name ?? callLog.number.toString(),
+        style: const TextStyle(
+          fontFamily: "Montserrat",
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      subtitle: Row(
+        children: [
+          Icon(
+            callTypeIcon,
+            color: const Color(0xffB2B8C2),
+            size: 15,
+          ),
+          const SizedBox(width: 5),
+          Text(
+            callType,
+            style: TextStyle(
+              color: callTypeColor,
+              fontFamily: "Montserrat",
+              fontSize: 14,
+            ),
+          ),
+          const SizedBox(width: 10),
+        ],
+      ),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            formattedTimestamp,
+            style: const TextStyle(
+                fontFamily: "Montserrat",
+                fontSize: 12,
+                color: Color(0xffB2B8C2)),
+          ),
+          const SizedBox(width: 10),
+          Container(
+            padding: const EdgeInsets.all(3),
+            decoration: BoxDecoration(
+              color: const Color(0xffECEEF2),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: const Icon(Icons.arrow_forward_ios,
+                size: 18, color: Color(0xffB2B8C2)),
+          ),
+        ],
+      ),
+      onTap: () {
+        // Define what happens when the tile is clicked
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,24 +159,56 @@ class _CallLogScreenState extends State<CallLogScreen> {
           });
         },
       ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color(0xff1B64FF),
+        onPressed: () {
+          //Contacts add function
+        },
+        child: const Icon(
+          Icons.call,
+          color: Colors.white,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       appBar: AppBar(
-        title: Text('Call Logs'),
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Container(
+          margin: const EdgeInsetsDirectional.only(top: 20),
+          height: 44,
+          decoration: BoxDecoration(
+              color: const Color(0xffEFF1F8),
+              borderRadius: BorderRadius.circular(20)),
+          child: TextField(
+            decoration: const InputDecoration(
+              hintText: 'Search names, numbers...',
+              hintStyle: TextStyle(
+                  fontFamily: "Montserrat",
+                  fontSize: 16,
+                  color: Color(0xff74777F)),
+              border: InputBorder.none,
+              prefixIcon: Icon(
+                Icons.search,
+                color: Colors.grey,
+              ),
+            ),
+            onChanged: (value) {
+              // Implement search functionality
+            },
+          ),
+        ),
       ),
       body: _callLogs.isNotEmpty
           ? ListView.separated(
               itemCount: _callLogs.length,
-              separatorBuilder: (context, index) => Divider(),
+              separatorBuilder: (context, index) => const Divider(),
               itemBuilder: (context, index) {
                 final callLog = _callLogs[index];
-                return ListTile(
-                  title: Text(callLog.name ?? 'Unknown'),
-                  subtitle: Text(callLog.number.toString()),
-                  trailing: Text(callLog.timestamp.toString()),
-                  // You can display more information about the call log if needed
-                );
+                return _buildCallLogTile(callLog);
               },
             )
-          : Center(
+          : const Center(
               child: Text('No call logs'),
             ),
     );
