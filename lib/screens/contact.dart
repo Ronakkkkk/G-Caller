97ed02eea13be73e2eb27ba73062d87e9d033755
Ronak <ronak01.raj@gmail.com>
2024-04-08 03:54:51 +0545

    update

--- lib/screens/contact.dart
+++ lib/screens/contact.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_contacts/flutter_contacts.dart';
+import 'package:gcaller/constants/colors.dart';
 import 'package:gcaller/widgets/bottom_navigation_bar.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -11,7 +12,9 @@ class ContactScreen extends StatefulWidget {
 }
 
 class _ContactScreenState extends State<ContactScreen> {
+  int _selectedIndex = 0;
   List<Contact> _contacts = [];
+  TextEditingController _searchController = TextEditingController();
 
   @override
   void initState() {
@@ -40,42 +43,136 @@ class _ContactScreenState extends State<ContactScreen> {
     }
   }
 
+  Widget _buildContactTile(Contact contact) {
+    final phoneNumbers = contact.phones.map((phone) => phone.number).toList();
+    final phoneText =
+        phoneNumbers.isNotEmpty ? phoneNumbers.join(', ') : 'No phone number';
+    return ListTile(
+      leading: CircleAvatar(
+        child: contact.photo != null
+            ? Image.memory(contact.photo!)
+            : Icon(Icons.person_outlined),
+      ),
+      title: Text(
+        contact.displayName ?? '',
+        style: const TextStyle(
+            fontFamily: "Montserrat",
+            fontSize: 16,
+            color: kPrimaryColor,
+            fontWeight: FontWeight.w500),
+      ),
+      onTap: () {
+        // Define what happens when the tile is clicked
+      },
+    );
+  }
+
+  List<Widget> _buildContactList() {
+    List<Widget> contactTiles = [];
+    String? prevChar;
+
+    _contacts.forEach((contact) {
+      final firstChar = contact.displayName?[0].toUpperCase();
+      if (prevChar != firstChar) {
+        contactTiles.add(ListTile(
+          title: Text(
+            firstChar!,
+            style: const TextStyle(
+                fontFamily: "Montserrat",
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+                color: Color(0xff1B72C0)),
+          ),
+          dense: true,
+        ));
+        prevChar = firstChar;
+      }
+      contactTiles.add(_buildContactTile(contact));
+    });
+
+    return contactTiles;
+  }
+
   @override
   Widget build(BuildContext context) {
-    int _selectedindex = 0;
-    return Scaffold(
-      bottomNavigationBar: BottomNavBar(
-          selectedIndex: _selectedindex,
-          onItemSelected: (index) {
+    return SafeArea(
+      child: Scaffold(
+        bottomNavigationBar: BottomNavBar(
+          selectedIndex: _selectedIndex,
+          onItemSelected: (int index) {
             setState(() {
-              _selectedindex = index;
+              _selectedIndex = index;
             });
-          }),
-      appBar: AppBar(
-        title: Text('Contacts'),
-        automaticallyImplyLeading: false,
-      ),
-      body: _contacts.isNotEmpty
-          ? ListView.separated(
-              itemCount: _contacts.length,
-              separatorBuilder: (context, index) => Divider(),
-              itemBuilder: (context, index) {
-                final contact = _contacts[index];
-                final phoneNumbers =
-                    contact.phones.map((phone) => phone.number).toList();
-                final phoneText = phoneNumbers.isNotEmpty
-                    ? phoneNumbers.join(', ')
-                    : 'No phone number';
-                return ListTile(
-                  title: Text(contact.displayName ?? ''),
-                  subtitle: Text(contact.id),
-                  // You can display more information about the contact if needed
-                );
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Color(0xff1B64FF),
+          onPressed: () {
+            //Contacts add function
+          },
+          child: Icon(
+            Icons.person_add,
+            color: Colors.white,
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          elevation: 0,
+          backgroundColor: Colors.white,
+          title: Container(
+            margin: EdgeInsetsDirectional.only(top: 20),
+            height: 44,
+            decoration: BoxDecoration(
+                color: const Color(0xffEFF1F8),
+                borderRadius: BorderRadius.circular(20)),
+            child: TextField(
+              controller: _searchController,
+              decoration: const InputDecoration(
+                hintText: 'Search names, numbers...',
+                hintStyle: TextStyle(
+                    fontFamily: "Montserrat",
+                    fontSize: 16,
+                    color: Color(0xff74777F)),
+                border: InputBorder.none,
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: Colors.grey,
+                ),
+              ),
+              onChanged: (value) {
+                // Implement search functionality
               },
-            )
-          : Center(
-              child: Text('No contacts'),
             ),
+          ),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20, left: 20),
+              child: Text(
+                'Contacts',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Montserrat",
+                  color: kPrimaryColor,
+                ),
+              ),
+            ),
+            Expanded(
+              child: _contacts.isNotEmpty
+                  ? ListView(
+                      children: _buildContactList(),
+                    )
+                  : const Center(
+                      child: Text('No contacts'),
+                    ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
