879c8047311ab7ff6b58744411aff8c471e870bb
Ronak <ronak01.raj@gmail.com>
2024-04-09 16:03:43 +0545

    adding dependecies

--- android/app/src/main/kotlin/com/example/gcaller/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/gcaller/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.gcaller
 
+import android.content.Context
+import androidx.multidex.MultiDex
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
 }
