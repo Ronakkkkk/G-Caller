940c2876a9b50848f54b9f090c3e8977e3ebcba5
Ronak <ronak01.raj@gmail.com>
2024-10-17 17:02:34 +0545

    okto_sdk_migration

--- lib/okto/okto.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:okto_flutter_sdk/okto_flutter_sdk.dart';
-
-final okto = Okto(dotenv.env['CLIENT_API_KEY']!, BuildType.sandbox);
