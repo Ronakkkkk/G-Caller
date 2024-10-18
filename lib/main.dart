19dcd410a075f70f7174a5729e1cf33c4c1898f9
Ronak <ronak01.raj@gmail.com>
2024-10-18 17:44:20 +0545

    imports

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,25 @@ import 'package:gcaller/onboarding/rewardsplashscreen.dart';
 import 'package:gcaller/onboarding/signin.dart';
 import 'package:gcaller/onboarding/welcome.dart';
 import 'package:gcaller/screens/contact.dart';
+import 'package:gcaller/utils/okto.dart';
 import 'package:gcaller/views/phone_state_listener.dart';
+import 'package:gcaller/wallet/okto_wallet.dart';
+import 'package:gcaller/wallet/view_wallet_page.dart';
 import 'package:gcaller/widgets/contact_stream.dart';
 import 'package:gcaller/widgets/overlay.dart';
+import 'package:okto_flutter_sdk/okto_flutter_sdk.dart';
+import 'package:gcaller/utils/global.dart';
 
 void main() async {
+  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
+
+  Globals globals = Globals.instance;
+  okto = Okto(globals.getOktoApiKey(), globals.getBuildType());
+
+  // Print the API key for debugging (remove in production)
+  print("Okto API Key: ${globals.getOktoApiKey()}");
+  print("Build Type: ${globals.getBuildType()}");
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
@@ -23,9 +36,9 @@ void main() async {
   }
 
   // Check if the user is already signed in
-  User? user = FirebaseAuth.instance.currentUser;
-  Widget initialRoute = user != null ? ContactScreen() : WelcomeScreen();
+
   ;
+
   PhoneStateListener().initialize();
   runApp(MyApp(initialRoute: ContactScreen()));
 }
@@ -35,6 +48,10 @@ class MyApp extends StatelessWidget {
 
   const MyApp({Key? key, required this.initialRoute}) : super(key: key);
 
+  Future<bool> checkLoginStatus() async {
+    return await okto!.isLoggedIn();
+  }
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
@@ -43,7 +60,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Gcaller',
-      home: initialRoute,
+      home: FutureBuilder<bool>(
+        future: checkLoginStatus(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          } else {
+            bool isLoggedIn = snapshot.data ?? false;
+            if (isLoggedIn) {
+              return ContactScreen();
+            } else {
+              return ViewWalletPage();
+            }
+          }
+        },
+      ),
     );
   }
 }
